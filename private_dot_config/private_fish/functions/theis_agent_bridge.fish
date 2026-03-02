2171d6e6bc214a7277b2aa6d876ab69e5888760b
Isaias <isaiasl@protonmail.com>
2026-03-01 17:17:37 -0800

    Fixed error with theis_agent_bridge

---
+++ private_dot_config/private_fish/functions/theis_agent_bridge.fish
@@ -0,0 +1,15 @@
+# Roughly translated from https://www.rebelpeon.com/bitwarden-ssh-agent-on-wsl2/
+
+function theis_agent_bridge
+  if test -z "$SSH_AUTH_SOCK"
+      set -x SSH_AUTH_SOCK $HOME/.ssh/agent.sock
+  end
+
+  if not test -S "$SSH_AUTH_SOCK"
+    rm -f "$SSH_AUTH_SOCK"
+    setsid socat \
+      "UNIX-LISTEN:$SSH_AUTH_SOCK,fork" \
+      "EXEC:npiperelay.exe -ei -s //./pipe/openssh-ssh-agent,nofork" \
+      >/dev/null 2>&1 &
+  end
+end
